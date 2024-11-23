cf2fa3030a88a79691e417f1bdc87d3064587ebe
endernon <ender_non@outlook.com>
2024-11-23 14:19:34 +0000

    actually set up serde correctly

--- src/lib.rs
+++ src/lib.rs
@@ -1,66 +1,64 @@
 use serde::{Serialize,Deserialize};
 #[derive(Serialize, Deserialize, Eq, PartialEq, Debug, Clone)]
-#[serde()]
-pub struct McMeta {
-    pub pack: McMeta_Pack,
-    pub features: McMeta_Features,
-    pub filter: Option<McMeta_Filter>,
-    pub overlays: Option<McMeta_Overlays>,
-    pub language: Option<Vec<McMeta_Language_Vec>>
+pub struct Mcmeta {
+    pub pack: McmetaPack,
+    pub features: McmetaFeatures,
+    pub filter: Option<McmetaFilter>,
+    pub overlays: Option<McmetaOverlays>,
+    pub language: Option<Vec<McmetaLanguage>>
 }
-
         #[derive(Serialize, Deserialize, Eq, PartialEq, Debug, Clone)]
-        pub struct McMeta_Pack {
-            pub description: Vec<McMeta_Pack_Description_Vec>,
+        pub struct McmetaPack {
+            pub description: Vec<McmetaPackDescription>,
             pub pack_format: u8,
-            pub supported_formats: Option<McMeta_Pack_SupportedFormats>
+            pub supported_formats: Option<McmetaPackSupportedformats>
         }
                 #[derive(Serialize, Deserialize, Eq, PartialEq, Debug, Clone)]
-                pub struct McMeta_Pack_Description_Vec {
+                pub struct McmetaPackDescription {
                     pub text: String,
                     pub color: Option<String>
                 }
                 #[derive(Serialize, Deserialize, Eq, PartialEq, Debug, Clone)]
-                pub struct McMeta_Pack_SupportedFormats {
+                pub struct McmetaPackSupportedformats {
                     pub min_inclusive: u8,
                     pub max_inclusive: u8
                 }
 
         // I don't actually know wtf this looks like. I'm guessing based on minecraft wiki
         #[derive(Serialize, Deserialize, Eq, PartialEq, Debug, Clone)]
-        pub struct McMeta_Features {
+        pub struct McmetaFeatures {
             pub enabled: Vec<String>
         }
 
         // I don't what this looks like either
         #[derive(Serialize, Deserialize, Eq, PartialEq, Debug, Clone)]
-        pub struct McMeta_Filter {
-            pub block: Vec<McMeta_Filter_Block_Vec>
+        pub struct McmetaFilter {
+            pub block: Vec<McmetaFilterBlock>
         }
                 #[derive(Serialize, Deserialize, Eq, PartialEq, Debug, Clone)]
-                pub struct McMeta_Filter_Block_Vec {
+                pub struct McmetaFilterBlock {
                     pub Namespace: String,
                     pub Path: String
                 }
 
         #[derive(Serialize, Deserialize, Eq, PartialEq, Debug, Clone)]
-        pub struct McMeta_Overlays {
-            pub entries: Vec<McMeta_Overlays_Entries>
+        pub struct McmetaOverlays {
+            pub entries: Vec<McmetaOverlaysEntries>
         }
 
                 #[derive(Serialize, Deserialize, Eq, PartialEq, Debug, Clone)]
-                pub struct McMeta_Overlays_Entries {
-                    pub formats: McMeta_Overlays_Entries_Formats,
+                pub struct McmetaOverlaysEntries {
+                    pub formats: McmetaOverlaysEntriesFormats,
                     pub directory: String
                 }
-                #[derive(Serialize, Deserialize, Eq, PartialEq, Debug, Clone)]
-                pub struct McMeta_Overlays_Entries_Formats {
-                    pub min_inclusive: u8,
-                    pub max_inclusive: u8
-                }
+                        #[derive(Serialize, Deserialize, Eq, PartialEq, Debug, Clone)]
+                        pub struct McmetaOverlaysEntriesFormats {
+                            pub min_inclusive: u8,
+                            pub max_inclusive: u8
+                        }
 
         #[derive(Serialize, Deserialize, Eq, PartialEq, Debug, Clone)]
-        pub struct McMeta_Language_Vec {
+        pub struct McmetaLanguage {
             pub name: String,
             pub region: String,
             pub bidirectional: bool
